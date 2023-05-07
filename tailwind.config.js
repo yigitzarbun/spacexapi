bdb967bb1c7ddc1bb4160bfbc5c15ec032815093
Yigit Zarbun <yzarbun@gmail.com>
2023-05-07 20:23:42 +0300

    responsive tailwind file initiated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      xxl: { max: "1920" },
+      // => @media (max-width: 1920px) { ... }
+      xl: { max: "1280" },
+      // => @media (max-width: 1280px) { ... }
+      lg: { max: "850" },
+      // => @media (max-width: 850px) { ... }
+      md: { max: "540" },
+      // => @media (max-width: 540px) { ... }
+      sm: { max: "414" },
+      // => @media (max-width: 414px) { ... }
+      xs: { max: "393" },
+      // => @media (max-width: 393px) { ... }
+    },
   },
   plugins: [],
 };
